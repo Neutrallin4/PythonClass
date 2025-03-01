d07727350d23e990705a3926ea91c03695f48088
Neutral <neutralobs@gmail.com>
2025-03-01 21:07:51 +0330

    minor changes

--- 00-Introduction/game.py
+++ 00-Introduction/game.py
@@ -3,17 +3,18 @@ import random
 print("Hello!")
 user_name = input("What's your name? ")
 print("Let's play a game ", user_name, "!", sep="")
-print("I am going to choose a number between 1 and 100 and you are going to guess it.")
+print("I am going to choose a number between" + " 1 and 100 and you are going to guess it.")
 
 random_number = random.randint(1, 100)
-not_guessed = True
 
-while not_guessed:
+while True:
     user_guess = int(input("Guess a number: "))
-    if user_guess > random_number:
+    if user_guess == random_number:
+        break
+    elif user_guess > random_number:
         print("Too high!")
     elif user_guess < random_number:
         print("Too low!")
-    else:
-        print("You guessed it, well done!")
-        break
+
+print(f"Well done, the number is {random_number}")
+
